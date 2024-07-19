49d1dc7eb54ec90848fba2664d289d6471526295
jason <jason.tadsilva@gmail.com>
2024-07-19 17:25:54 +0900

    add docker

---
+++ docker/docker_install.sh
@@ -0,0 +1,9 @@
+# build
+docker build --tag benchmarl:v1.0 -f Dockerfile .
+
+# run
+docker run --gpus all -v "/home/$USER/xCode/BenchMARL:/BenchMARL" -itd --name benchmarl_v1_0 benchmarl:v1.0
+
+
+# exec docker
+docker exec -it benchmarl_v1_0 /bin/bash
